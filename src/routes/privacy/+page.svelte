d6aac8048e852d18f06a5c5030d569d9186f8c56
aaron <alkrauss48@gmail.com>
2024-08-12 15:21:32 -0500

    feat: add privacy page

---
+++ src/routes/privacy/+page.svelte
@@ -0,0 +1,27 @@
+<script>
+	import appStore from '$lib/images/download-on-the-app-store.svg';
+</script>
+
+<svelte:head>
+	<title>Privacy Policy</title>
+	<meta name="description" content="The Privacy Policy for the Lucas Hints iOS app." />
+</svelte:head>
+
+<div class="text-column">
+	<h1>Privacy Policy</h1>
+
+  <p>Last updated: August 12, 2024</p>
+
+	<p>
+    Lucas Hints does not collect any personal information. We do not store or
+    transmit your personal details, nor do we include any advertising or
+    analytics software that talks to third parties.
+	</p>
+
+  <h2>Contact</h2>
+
+  <p>
+    If you have any questions or concerns, please feel free to
+    <a href="mailto:info@lucaskrauss.dev">contact us</a>.
+  </p>
+</div>
