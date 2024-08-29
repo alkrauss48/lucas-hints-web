2dc33d7b34086709c8a3aedd269010747588a6a6
aaron <alkrauss48@gmail.com>
2024-08-29 09:38:15 -0500

    feat: add demo gif to home page
    Addresses #1

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script>
 	import logo from '$lib/images/lucas-hints.jpeg';
 	import appStoreImg from '$lib/images/download-on-the-app-store.svg';
+	import demoGif from '$lib/images/demo-lucas-hints.gif';
 
 	const appStoreUrl = 'https://apps.apple.com/app/lucas-hints/id6596782787';
 </script>
@@ -14,7 +15,7 @@
 </svelte:head>
 
 <section>
-	<img class="logo" src={logo} alt="Lucas Hints logo" />
+	<img class="logo mt-12" src={logo} alt="Lucas Hints logo" />
 
 	<h1>
 		Welcome to <span class="inline-block">Lucas Hints</span>
@@ -28,6 +29,8 @@
 		<strong>Lucas Hints</strong> is an iOS app that helps you create fun, memorable stories with friends
 		and family by kickstarting your creativity.
 	</p>
+
+	<img class="mt-12" src={demoGif} alt="Download Lucas Hints on the App Store" />
 </section>
 
 <style>
